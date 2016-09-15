25c1d38c5e557739278a1b5b5c69290d42665067
satya123zx <satyakam.mohapatar91@gmail.com>
2016-09-16 00:13:00 +0530

    added new method

---
+++ HelloWorldDemo/src/com/satya/controler/MainFirstServlet.java
@@ -0,0 +1,25 @@
+package com.satya.controler;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MainFirstServlet
+ */
+@WebServlet(description = "Testing doGet", urlPatterns = { "/MainFirstServlet" })
+public class MainFirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("In the MainFirstServlet doPost Method");
+	}
+
+}
