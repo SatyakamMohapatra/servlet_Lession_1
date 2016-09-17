eafacf1db392fd18ee3fc81d9a3922e81426b634
satyakam.m <satyakam.mohapatra91@gmailcom>
2016-09-17 12:38:34 +0530

    removed warning

--- HelloWorldDemo/src/com/satya/controler/MainControler.java
+++ HelloWorldDemo/src/com/satya/controler/MainControler.java
@@ -3,16 +3,12 @@ package com.satya.controler;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.Servlet;
-import javax.servlet.ServletConfig;
+
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.sun.net.httpserver.HttpsServer;
 
 public class MainControler extends HttpServlet {
 
