b5df447fd0ef050987ff2a35013d03c11db81ca2
satya123zx <satyakam.mohapatar91@gmail.com>
2016-09-19 00:55:23 +0530

    Session Tracking Added------HttpSesion();

--- HelloWorldDemo/src/com/satya/controler/LoginControler.java
+++ HelloWorldDemo/src/com/satya/controler/LoginControler.java
@@ -5,50 +5,67 @@ import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import com.satya.service.LoginServiceImpl;
 
 /**
  * Servlet implementation class LoginControler
  */
-/*@WebServlet("/LoginControler")*/
+/* @WebServlet("/LoginControler") */
 public class LoginControler extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	LoginServiceImpl loginService;
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("[LoginControler]..[doPost]..[Getting Login Request].."
-				+ ""+"[Thread name-]-"+Thread.currentThread().getName()); 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("[LoginControler]..[doPost]..[Getting Login Request].." + "" + "[Thread name-]-"
+				+ Thread.currentThread().getName());
 		loginService = new LoginServiceImpl();
 		String user = request.getParameter("user");
+		HttpSession session = request.getSession();
+		System.out.println("Print Session ID preseance "+session.isNew());
+		System.out.println("Print Session ID preseance "+session.getId());
+		HttpSession session1 = request.getSession(false);
+		System.out.println("Print Session ID preseance session1 "+session1.getId());
+		System.out.println("Print Session ID preseance session1 "+session);
+		session.setAttribute("name", "satya");
+		System.out.println("Print Session ID preseance session1 "+session.getAttribute("name"));
+		session.invalidate();
+		//System.out.println("Print Session ID preseance session1 "+session.getAttribute("name"));
 		String pass = request.getParameter("Password");
-		System.out.println("Entered User is " +user+" and Password is "+pass);
+		System.out.println("Entered User is " + user + " and Password is " + pass);
 		request.setAttribute("user", "satyakamMohapatra");
 		PrintWriter writer = response.getWriter();
-		if(user.contentEquals("satya")){
-		if(loginService.setUser(user, pass)==1){
-			System.out.println("User Account Created");
-			writer.write("Your User Account Created "+ user);
-			RequestDispatcher dispatcher = request.getRequestDispatcher("MainFirstServlet");
-			dispatcher.forward(request, response);
-			System.out.println("User Account Sucessfull");
-			System.out.println("Request Attribute Remove [LoginControler] Example :-"+request.getAttribute("user"));
-		}else{
-			System.out.println("Issue while creating user account");
-		}
-		}else{
+		if (user.contentEquals("satya")) {
+			if (loginService.setUser(user, pass) == 1) {
+				System.out.println("User Account Created");
+				writer.write("Your User Account Created " + user);
+				Cookie cookie = new Cookie("username", user);
+				response.addCookie(cookie);
+				RequestDispatcher dispatcher = request.getRequestDispatcher("MainFirstServlet");
+				dispatcher.forward(request, response);
+				System.out.println("User Account Sucessfull");
+				System.out
+						.println("Request Attribute Remove [LoginControler] Example :-" + request.getAttribute("user"));
+			} else {
+				System.out.println("Issue while creating user account");
+			}
+		} else {
 			writer.write("<h3>Enter a valid user</h3>");
 			System.out.println("Issue while creating user account");
 			RequestDispatcher dispatcher = request.getRequestDispatcher("Login.jsp");
 			dispatcher.include(request, response);
 		}
-		
-		
+
 	}
 
 }
