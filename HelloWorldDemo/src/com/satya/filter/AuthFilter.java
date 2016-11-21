7f78ac187c742b63f6c2e1d0b45b20cbd6eabf94
satyakam.m <satyakam.mohapatra91@gmailcom>
2016-11-21 13:02:07 +0530

    ==servlet commit

--- HelloWorldDemo/src/com/satya/filter/AuthFilter.java
+++ HelloWorldDemo/src/com/satya/filter/AuthFilter.java
@@ -9,6 +9,9 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class AuthFilter implements Filter {
 
@@ -21,19 +24,12 @@ public class AuthFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		//System.out.println("[com.satya.filter.AuthFilter][AuthFilter][doFilter] inside checker 1...");
-		//RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/MainTest.jsp");
-		//dispatcher.include(request, response);
-	    String s=request.getParameter("user");
-		System.out.println("[com.satya.filter.AuthFilter][AuthFilter][doFilter] in filter...");
-	    //chain.doFilter(request, response);
-		//if (s.equals("satya")) {
-			System.out.println("[com.satya.filter.AuthFilter][AuthFilter][doFilter] inside checker 2...");
-			chain.doFilter(request, response);
-			System.out.println("[com.satya.filter.AuthFilter][AuthFilter][doFilter] post filter...");
-		//}else{
-			System.out.println("[com.satya.filter.AuthFilter][AuthFilter][doFilter] password did not match...");
-	//	}
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		HttpSession session = req.getSession();
+		System.out.println("[session][doFilter] "+session);
+		if (session )
 	}
 
 	@Override
